5a8d06a7c8efa17e2f5b7516c1fd741bde02c72d
Granit Halimi <granithalimi@Granits-MacBook-Air.local>
2025-08-17 05:38:27 +0200

    designed auth

--- app/auth/sign-up-success/page.tsx
+++ app/auth/sign-up-success/page.tsx
@@ -8,8 +8,9 @@ import {
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-tl from-gray-900 to-gray-600">
+      <div className="absolute inset-0 bg-black/50"></div>
+      <div className="w-full max-w-sm z-10">
         <div className="flex flex-col gap-6">
           <Card>
             <CardHeader>
--- app/auth/update-password/page.tsx
+++ app/auth/update-password/page.tsx
@@ -2,8 +2,9 @@ import { UpdatePasswordForm } from "@/components/update-password-form";
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-tl from-gray-900 to-gray-600">
+      <div className="absolute inset-0 bg-black/50"></div>
+      <div className="w-full max-w-sm z-10">
         <UpdatePasswordForm />
       </div>
     </div>
