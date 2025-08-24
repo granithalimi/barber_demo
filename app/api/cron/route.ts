d4932bfc7af0048ef6f5866dcfd8d7d6ed2d9687
Granit Halimi <halimigranit6@gmail.com>
2025-08-24 03:42:39 +0200

    going back in cron jobs

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,14 +1,19 @@
-import type { NextRequest } from 'next/server';
+// import type { NextRequest } from 'next/server';
  
-export function GET(request: NextRequest) {
-  const authHeader = request.headers.get('authorization');
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response('Unauthorized', {
-      status: 401,
-    });
-  }
+// export function GET(request: NextRequest) {
+//   const authHeader = request.headers.get('authorization');
+//   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+//     return new Response('Unauthorized', {
+//       status: 401,
+//     });
+//   }
+//
+//   console.log("Cron ran at:", new Date())
+//
+//   return Response.json({message: "Cron ran at: " + new Date()});
+// }
 
-  console.log("Cron ran at:", new Date())
- 
-  return Response.json({message: "Cron ran at: " + new Date()});
+export function GET() {
+  console.log("Cron ran at:", new Date().toISOString())
+  return Response.json({message: "Cron ran at: " + new Date().toISOString()});
 }
